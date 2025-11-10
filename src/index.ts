9cf2620b0dd522bb094e1d7e5b8784859943ebd8
Tanadon Hunsadee <tanadonhun.dev@gmail.com>
2025-11-11 00:34:09 +0700

    remove service uploadSservice

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import cors from "cors";
 import productRoutes from "./routes/productRoute";
+import path from "path";
 
 const app = express();
 
-// ✅ ตั้งค่า CORS
 app.use(cors({
-  origin: "http://localhost:3000", // อนุญาตเฉพาะ frontend ของคุณ
+  origin: "http://localhost:3000",
 }));
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 3333; // กำหนด default port เผื่อ env ไม่มีค่า
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,15 +1,13 @@
 import { db } from "../db";
 import { product, productImage } from "../db/schema";
-import { handleFileUpload } from "./uploadService";
 
 export async function createProduct(req: any) {
-  const { files, formData } = await handleFileUpload(req);
   const result = await db
     .insert(product)
-    .values([{ title: formData.title, price: formData.price }])
+    .values([{ title: req.body.title, price: req.body.price }])
     .$returningId();
 
-  for (const file of files) {
+  for (const file of req.body.images) {
     await db.insert(productImage).values({
       productId: result[0].id,
       imageName: file,
